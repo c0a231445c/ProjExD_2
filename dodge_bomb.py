b0ee80273f6dbc9e214876cbe9561191a71d6841
C0A23144 <c0a231445c@edu.teu.ac.jp>
2024-06-18 14:45:37 +0900

    [200~

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -63,6 +63,11 @@ def main():
         screen.blit(kk_img, kk_rct)
 
         bb_rct.move_ip(vx, vy)
+        yoko, tate = check_bound(bb_rct)
+        if not yoko:
+            vx *= -1
+        if not tate:
+            vy *= -1
         screen.blit(bb_img, bb_rct)
         pg.display.update()
         tmr += 1
